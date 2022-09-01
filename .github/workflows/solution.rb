edee3c9e75fc42bfdc7c186ff4f3c0ab615c4810
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-01 10:52:11 +0100

    Add github work flow

---
+++ .github/workflows/solution.rb
@@ -0,0 +1,49 @@
+@alphabets = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '..--' => 'Z'
+}
+
+def decode_char(character)
+  @alphabets[character]
+end
+
+def decode_word(word)
+  decoder = ''
+  character = word.split
+  character.each { |c| decoder += decode_char(c) }
+  decoder
+end
+
+def decode_message(message)
+  decoder_message = ''
+  words = message.split(/   /)
+  words.each { |c| decoder_message += "#{decode_word(c)} " }
+  decoder_message
+end
+
+ puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+ 
